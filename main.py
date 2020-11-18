29f42807eff458b7508106850a70d1b98af1d76f
vinayakhegde1 <73926849+vinayakhegde1@users.noreply.github.com>
2020-11-18 14:36:01 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+"""num1 = int(input(), 2)
+num2 = int(input(), 2)
+answer = num1 + num2
+answerString = bin(answer)[2:]
+print(answerString)"""
+
+"""num1 = int(input(), 2)
+num2 = int(input(), 2)
+answer = num1 - num2
+answerString = bin(answer)[2:]
+print(answerString)"""
